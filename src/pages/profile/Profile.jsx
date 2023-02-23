7ac6178f93b216a5aeb94c453c17f9815427cce9
Rezi <105070638+rezi410@users.noreply.github.com>
2023-02-23 05:32:13 -0500

    Update Profile.jsx

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,20 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./profile.scss"
 
-const ResetPassword = () => {
+const Profile = () => {
+    const navigate = useNavigate();
+    const handleClick = (e) => {
+      localStorage.removeItem('user');
+      navigate('/login');
+    }
+    useEffect(() => {
+      if (!localStorage.getItem('user')) {
+        navigate('/login');
+      }
+    }, [navigate]);
     return (
-        <div className="ResetPassword">
+        <div className="Profile">
         <div className="card">
           <div className="left">
             <h1>Profile</h1>
@@ -13,6 +24,7 @@ const ResetPassword = () => {
             <Link to='/resetPassword'>
             <h2>Change Password</h2>
             </Link>
+            <button onClick={handleClick}>Loggout</button>
           </div>
           <div className="right">
             <h1>(Coming Soon!!)</h1>
@@ -22,4 +34,4 @@ const ResetPassword = () => {
     );
 }
 
-export default ResetPassword;
+export default Profile;
